f6d05bfe581acd901a18094c725c6babcaf36818
Dany <danycheong24@gmail.com>
2017-02-04 20:07:16 -0600

    service change

--- app/scripts/services/BusinessService.js
+++ app/scripts/services/BusinessService.js
@@ -1,6 +1,6 @@
 angular.module('MyApp.Services').factory('BusinessService', ['$http',
 	function($http){
-		var baseUrl = 'http://ubuntu-dilo-ya:8000/';
+		var baseUrl = 'https://dilo-ya.com/app2';
 		//var baseUrl = 'https://dilo-ya-backend.herokuapp.com/';
 		return {
 			All: function() {
